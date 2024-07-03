fe5c7004e4181e242aeb05f69c61c942223a35b9
suny <me@suny.cloud>
2024-07-03 12:08:08 +0800

    feat: Add RSS icon to socialIcons.ts and enable RSS feed link in config.ts

--- src/config.ts
+++ src/config.ts
@@ -144,4 +144,10 @@ export const SOCIALS: SocialObjects = [
     linkTitle: `${SITE.title} on Mastodon`,
     active: false,
   },
+  {
+    name: "RSS",
+    href: "/rss.xml",
+    linkTitle: "RSS Feed",
+    active: true,
+  },
 ];
